6c7a1e0eab85916c3c4ea3092c9af47c0fd7d573
Cristian Richard Kulessa <42663268+Kulessa@users.noreply.github.com>
2023-10-31 17:19:32 -0300

    newUser.Active = (role != StaticUserRoles.PROMOTER);

--- src/TicketNow/TicketNow.Service/Services/AuthService.cs
+++ src/TicketNow/TicketNow.Service/Services/AuthService.cs
@@ -76,7 +76,7 @@ namespace TicketNow.Service.Services
             var newUser = _mapper.Map<User>(registerDto);
 
             newUser.CreatedAt = DateTime.Now;
-            newUser.Active = ((role == StaticUserRoles.CUSTOMER) || (role == StaticUserRoles.ADMIN && registerDto.Username == "admin"));
+            newUser.Active = (role != StaticUserRoles.PROMOTER);
 
             var createUserResult = await _userManager.CreateAsync(newUser, registerDto.Password);
 
